bc7253713567a95c5264aa4f5de81b64e3c08314
YZouad <yaniszouad07@gmail.com>
2023-08-04 15:30:17 -0400

    w-full fix

--- pages/members/index.js
+++ pages/members/index.js
@@ -34,7 +34,7 @@ const AllMembersPage = ({members}) => {
              <div key={member.fields.slug} className="card-default flex flex-col overflow-hidden rounded-lg shadow-lg mb-5 ml-0 md:ml-5 hover:bg-gray-100">
              <a href={'/members/' + member.fields.slug}> 
             <div className="flex-shrink-0 w-full md:w-[300px]">
-              <img className="h-64 object-cover overflow-hidden" src={'https://'+member.fields.image.fields.file.url} alt=""></img>
+              <img className="h-64 object-cover overflow-hidden w-full" src={'https://'+member.fields.image.fields.file.url} alt=""></img>
             </div>
             <div className="flex flex-1 flex-col justify-between p-6">
               <div className="flex-1">
